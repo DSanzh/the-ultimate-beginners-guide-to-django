2a90cd80534cbd3f3f58c540840462bcd132a56b
Sanzhar <dauylov.sanzhar@gmail.com>
2017-11-18 19:38:36 +0600

    Finish 3rd project: Reddit Clone

--- redditclone/accounts/views.py
+++ redditclone/accounts/views.py
@@ -1,11 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        registerUser()
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.get(username=request.POST["username"])
+                return render(request, 'accounts/signup.html', {'error': "Username has already been used"})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST["password1"])
+                login(request, user)
+                return render(request, 'accounts/signup.html')
+        else:
+            return render(request, 'accounts/signup.html', {'error': "Passwords didn't match"})
     else:
         return render(request, 'accounts/signup.html')
 
-def registerUser():
-    print('register user')
+def loginView(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST['next'])
+            else:
+                return render(request, 'accounts/login.html', {"error":"Login is successfull"})
+        else:
+            return render(request, 'accounts/login.html', {"error":"The username or password is incorrect"})
+    else:
+        return render(request, 'accounts/login.html')
+
+def logoutView(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
